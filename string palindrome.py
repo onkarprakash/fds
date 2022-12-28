e2a2f6bf071410e5d33d5761fdb433ba63587870
onkarprakash <121566651+onkarprakash@users.noreply.github.com>
2022-12-28 21:52:19 +0530

    Add files via upload

--- string palindrome.py
+++ string palindrome.py
@@ -20,6 +20,10 @@ print("Count of all characters in sentence is :\n "
       + str(all_freq))
 
 
+
+
+# c) To check whether given string is palindrome or not
+
 text="English"
 print("Given text is "+text)
 rev=reversed(text)
@@ -27,6 +31,10 @@ if list(text)==list(rev):
     print("its a palindrome")
 else:
     print("its not a palindrome")
+
+
+
+# d) To display index of first appearance of the substring
 
 
 sub_str1 =str(input("Enter word"))
@@ -40,20 +48,30 @@ else:
     print("Substring found")
 
 
+
+
+#  e) To count the occurrences of each word in a given string.
+
 print("Following are the count the frequency of each word in a given string")
 def freq(sentence):
     # break the string into list of words
     sentence = sentence.split()
     str2 = []
 
+    # loop till string values present in list str
     for i in sentence:
+
+        # checking for the duplicacy
         if i not in str2:
+            # insert value in str2
             str2.append(i)
 
     for i in range(0, len(str2)):
+        # count the frequency of each word(present in str2) in sentence and print
         print('count of frequency', str2[i], 'is :', sentence.count(str2[i]))
 
 def main():
+    # call freq function
     freq(sentence)
 
-main() 
+main()  # call main function
